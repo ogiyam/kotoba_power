220906015ae77a1b0e45291d068ff26ad04beafc
ogiyam <miyuogym@gmail.com>
2021-11-01 17:04:40 +0000

    [Add]ログイン後マイページ遷移

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,13 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-  
+
   # deviseにnameカラム追加
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def after_sign_in_path_for(resource)
+    user_path(@user)
   end
 end
