0af5540d205318357cb3c3f8318a821183139d9f
ogiyam <miyuogym@gmail.com>
2021-11-01 15:52:26 +0000

    [Add]deviseカスタマイズ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-
-　# deviseにnameカラム追加
+  
+  # deviseにnameカラム追加
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
