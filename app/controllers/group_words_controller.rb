2e408c687baa26c7a9fea2f4c1e5f04d1ee31c94
ogiyam <miyuogym@gmail.com>
2021-11-04 13:09:01 +0000

    [Add]group_wordコントローラー

--- app/controllers/group_words_controller.rb
+++ app/controllers/group_words_controller.rb
@@ -1,3 +1,39 @@
 class GroupWordsController < ApplicationController
-  
+
+  def index
+    @groups = Group.find(params[:group_id])
+    @group_words = GroupWord.where(group_id: @group_id)
+  end
+
+  def new
+    @group = Group.find(params[:group_id])
+    @group_word = GroupWord.new
+  end
+
+  def create
+    @group = Group.find(params[:group_id])
+    @group_word = GroupWord.new(group_word_params)
+    if @group_word.save
+      redirect_to group_groupword_path, notice: "ことばを追加しました"
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @group_word = GroupWord.find(params[:id])
+    if @group.owner_id == current_user.id
+      @group_word.destroy
+      redirect_to group_groupword_path, notice: "ことばを削除しました"
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def group_word_params
+    params.require(:group_word).permit(:user_id, :group_id, :word)
+  end
 end
--- app/models/group_word.rb
+++ app/models/group_word.rb
@@ -1,6 +1,6 @@
 class GroupWord < ApplicationRecord
-  belongs_to :user
-  belongs_to :group
-  
+  belongs_to :user, optional: true
+  belongs_to :group, optional: true
+
   validates :word, presence: true
 end
