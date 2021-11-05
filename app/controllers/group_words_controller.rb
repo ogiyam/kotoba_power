ca18179973f48d4abc72980d693dfaf0f996ca00
ogiyam <miyuogym@gmail.com>
2021-11-05 05:19:48 +0000

    [Fix]group_wordコントローラー

--- app/controllers/group_words_controller.rb
+++ app/controllers/group_words_controller.rb
@@ -1,7 +1,10 @@
 class GroupWordsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:new, :create]
 
   def index
-    @groups = Group.find(params[:group_id])
+    @group = Group.find(params[:group_id])
+    @group_word = @group.group_words.includes(:group_id)
     @group_words = GroupWord.where(group_id: @group_id)
   end
 
@@ -14,8 +17,9 @@ class GroupWordsController < ApplicationController
     @group = Group.find(params[:group_id])
     @group_word = GroupWord.new(group_word_params)
     if @group_word.save
-      redirect_to group_groupword_path, notice: "ことばを追加しました"
+      redirect_to  group_groupword_index_path(@group), notice: "ことばを追加しました"
     else
+      @group_word = GroupWord.where(group_id: @group_id)
       render 'new'
     end
   end
@@ -25,7 +29,7 @@ class GroupWordsController < ApplicationController
     @group_word = GroupWord.find(params[:id])
     if @group.owner_id == current_user.id
       @group_word.destroy
-      redirect_to group_groupword_path, notice: "ことばを削除しました"
+      redirect_to  group_groupword_index_path, notice: "ことばを削除しました"
     else
       render 'index'
     end
@@ -34,6 +38,13 @@ class GroupWordsController < ApplicationController
   private
 
   def group_word_params
-    params.require(:group_word).permit(:user_id, :group_id, :word)
+    params.require(:group_word).permit(:word, :group_id)
+  end
+
+  def ensure_correct_user
+    @group = Group.find(params[:group_id])
+    unless @group.owner_id == current_user.id
+      redirect_to group_groupword_index_path
+    end
   end
 end
