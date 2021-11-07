3ddd47a642443731c147f7c6ecdd257ddb1ebb32
ogiyam <miyuogym@gmail.com>
2021-11-07 14:58:24 +0000

    [Add]no_opinion非同期通信

--- app/models/group_word.rb
+++ app/models/group_word.rb
@@ -2,10 +2,15 @@ class GroupWord < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :group, optional: true
   has_many :yes_opinions, dependent: :destroy
+  has_many :no_opinions, dependent: :destroy
 
   validates :word, presence: true
   
   def yes_opinions_by?(user)
     yes_opinions.where(user_id: user.id).exists?
+  end
+  
+  def no_opinions_by?(user)
+    no_opinions.where(user_id: user.id).exists?
   end
 end
